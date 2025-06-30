df51da662cf39e55a620096ac8bf6e5bcb475612
dallen97 <101841823+dallen97@users.noreply.github.com>
2025-06-30 18:46:23 -0400

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import express from 'express';     
 import bcrypt from 'bcrypt';       
 import User from '../model/users.js'; 
@@ -6,11 +7,11 @@ const router = express.Router();
 
 router.post('/login', async (req, res) => { 
     let error = ''; 
-
     const { login, password } = req.body;
 
     let id = -1;       
-    let username = ''; 
+    let username = '';
+    let token = '';
 
     try {
         const user = await User.findOne({ login: login.toLowerCase() });
@@ -18,12 +19,17 @@ router.post('/login', async (req, res) => {
         if (!user) {
             error = 'Invalid user name/password';
         } else {
-            // Compare the provided password with the hashed password stored in the database
             const isMatch = await bcrypt.compare(password, user.password);
 
             if (isMatch) {
                 id = user._id;
                 username = user.username;
+
+                token = jwt.sign(
+                    { userId: user._id, username: user.username },
+                    process.env.JWT_SECRET,
+                    { expiresIn: '1h' };
+                )
             } else {
                 error = 'Invalid user name/password';
             }
@@ -31,14 +37,16 @@ router.post('/login', async (req, res) => {
     } catch (err) {
         console.error('Login error:', err);
         error = 'Server error during login process.';
-        return res.status(500).json({ id: -1, username = '', error: error });
+        return res.status(500).json({ id: -1, username = '', token = '', error: error });
     }
 
     const ret = {
-        id: id,
-        username: username;
-        error: error
+        id,
+        username,
+        token,
+        error
     };
+    
     res.status(200).json(ret);
 });
 
