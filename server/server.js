36dca7bf6a979ced18c8c8668b43a2ed519ba121
dallen97 <101841823+dallen97@users.noreply.github.com>
2025-06-30 19:25:09 -0400

    Update server.js
    updated to include register, login and resume functionality

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import resumeRoutes from "./api/resumeRoutes.js";
+import registerRoute from "./api/register.js";
+import loginRoute from "./api/login.js";
 
 dotenv.config();
 const app = express();
@@ -12,6 +15,10 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
+
+app.use("/api/register", registerRoute);
+app.use("/api/login", loginRoute);
+app.use("/api/resumes", resumeRoutes);
 
 app.get("/", (req, res) => res.send("API running"));
 app.listen(3000, () => console.log("Server running on port 3000!!:"));
