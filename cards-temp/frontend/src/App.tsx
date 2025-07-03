d87a9f49a7304bb4b222cc210db705dd5dfe4bdc
abduk9876 <abdullahkabalaoui@gmail.com>
2025-07-02 21:38:20 -0400

    reupload MERN C + register

---
+++ cards-temp/frontend/src/App.tsx
@@ -0,0 +1,20 @@
+//import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
+import LoginPage from './pages/LoginPage';
+import CardPage from './pages/CardPage';
+import RegisterPage from './pages/RegisterPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path="/cards" element={<CardPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ cards-temp/frontend/src/components/Register.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { buildPath } from './Path';
+
+function Register()
+{
+    const [message,setMessage] = useState('');
+    const [first,setFirst] = React.useState('');
+    const [last,setLast] = React.useState('');
+    const [loginName,setLoginName] = React.useState('');
+    const [loginPassword,setPassword] = React.useState('');
+
+    async function doRegister(event:any) : Promise<void>
+    {
+        event.preventDefault();
+
+        var obj = {firstName:first,lastName:last,login:loginName,password:loginPassword};
+        var js = JSON.stringify(obj);
+        
+        try
+        {
+            const response = await fetch(buildPath('api/register'), {method:'POST',body:js,headers:{'Content-Type':'application/json'}});
+            
+            var res = JSON.parse(await response.text());
+
+            if( res.error.length > 0 )
+            {
+                setMessage( "API Error:" + res.error );
+            }
+            else
+            {
+                setMessage('User has been created');
+            }
+        }
+        catch(error:any)
+        {
+            setMessage(error.toString());
+        }
+    };
+
+    function handleSetFirst(e:any):void
+    {
+        setFirst(e.target.value);
+    }
+
+    function handleSetLast(e:any):void
+    {
+        setLast(e.target.value);
+    }
+    
+    function handleSetLoginName( e: any ) : void
+    {
+        setLoginName( e.target.value );
+    }
+    
+    function handleSetPassword( e: any ) : void
+    {
+        setPassword( e.target.value );
+    }
+    
+    return(
+        <div id="registerDiv">
+            <span id="inner-title">PLEASE REGISTER</span><br />
+            First Name: <input type="text" id="first" placeholder="First Name"
+                onChange={handleSetFirst} /><br />
+            Last Name: <input type="text" id="last" placeholder="Last Name"
+                onChange={handleSetLast} /><br />
+            Login: <input type="text" id="loginName" placeholder="Username"
+                onChange={handleSetLoginName} /><br />
+            Password: <input type="password" id="loginPassword" placeholder="Password"
+                onChange={handleSetPassword} /><br />
+            <input type="submit" id="registerButton" className="buttons" value = "Register"
+                onClick={doRegister} /><br />
+            <span id="registerResult">{message}</span>
+        </div>
+    );
+};
+
+export default Register;
