3654d94dcb39e344fe44f393f1b1d0e79e09848e
abduk9876 <abdullahkabalaoui@gmail.com>
2025-07-09 18:53:21 -0400

    small frontend changes

---
+++ cards/frontend/src/pages/ResumePage.tsx
@@ -0,0 +1,15 @@
+import PageTitle from '../components/PageTitle';
+import LoggedInName from '../components/LoggedInName';
+//import ResumeUI from '../components/ResumeUI';
+
+const ResumePage = () =>
+{
+    return(
+        <div>
+            <PageTitle />
+            <LoggedInName />
+        </div>
+    );
+}
+
+export default ResumePage;
